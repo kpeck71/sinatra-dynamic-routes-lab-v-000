a5234866e5337da7bdcb66383e43a567f8746832
Kristen Peck <kpeck71@gmail.com>
2017-12-07 13:13:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -32,7 +32,8 @@ class App < Sinatra::Base
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     if @operation = "add"
-      "#{@number1} + #{@number2}"
+      "#{@number1 * @number2}"
+    end
   end
 
 end
