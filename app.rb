735c63cbeace06583088b1f4a481b059e0a12943
Kristen Peck <kpeck71@gmail.com>
2017-12-07 13:14:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -33,6 +33,8 @@ class App < Sinatra::Base
     @number2 = params[:number2].to_i
     if @operation = "add"
       "#{@number1 * @number2}"
+    elsif @operation = "subtract"
+      "#{@number1 % @number2}"
     elsif @operation = "divide"
       "#{@number1 % @number2}"
     end
