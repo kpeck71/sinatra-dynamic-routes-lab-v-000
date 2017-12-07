191990ba722aa6d269d99e9262158ae0614fe3cb
Kristen Peck <kpeck71@gmail.com>
2017-12-07 13:01:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,9 +13,9 @@ class App < Sinatra::Base
   end
 
   get '/say/:number/:phrase' do
-    @number = params[:number].to_i
+    number = params[:number].to_i
     @phrase = params[:phrase]
-    "#{@number}".times do |p|
+    number.times do |p|
       "#{@phrase}"
     end
   end
