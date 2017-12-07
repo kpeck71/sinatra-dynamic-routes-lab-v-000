e91ff18511f49df3d63e352c0e2fc57c91e92f2c
Kristen Peck <kpeck71@gmail.com>
2017-12-07 13:18:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation].to_s
+    @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     if @operation == "add"
