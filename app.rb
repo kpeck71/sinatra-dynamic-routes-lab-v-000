0d6316082aae2194d3a68d6d584aaaf06d6e3384
Kristen Peck <kpeck71@gmail.com>
2017-12-07 13:12:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     @phrase = params[:phrase]
     "#{@phrase} " * @number
   end
-  
+
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @word1 = params[:word1]
     @word2 = params[:word2]
@@ -25,6 +25,11 @@ class App < Sinatra::Base
     @word4 = params[:word4]
     @word5 = params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
   end
 
 end
