4f57766b3210a8d4a143d96f76abfc45916cdb75
Kristen Peck <kpeck71@gmail.com>
2017-12-07 13:05:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,11 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    x=0
-    while x >= @number
-      "#{@phrase}"
-      x+=1
-    end
+    
   end
 
 end
